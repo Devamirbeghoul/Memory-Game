17c75ed3c5f3783a878a367824b137f9076b24f4
Amir Abdeldjalil <kzero3187@gmail.com>
2024-09-17 20:10:14 +0100

    End Work

---
+++ Memory Game/Memory Game Screen.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Memory_Game
+{
+    partial class fmMemoryGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmMemoryGame));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lable5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.lbMoves = new System.Windows.Forms.Label();
+            this.btnStart = new Guna.UI2.WinForms.Guna2Button();
+            this.btnRestart = new Guna.UI2.WinForms.Guna2Button();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(52)))), ((int)(((byte)(95)))));
+            this.label1.Location = new System.Drawing.Point(226, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Memory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(52)))), ((int)(((byte)(95)))));
+            this.label2.Location = new System.Drawing.Point(415, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 51);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Game";
+            // 
+            // lable5
+            // 
+            this.lable5.AutoSize = true;
+            this.lable5.BackColor = System.Drawing.Color.Transparent;
+            this.lable5.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lable5.ForeColor = System.Drawing.Color.White;
+            this.lable5.Location = new System.Drawing.Point(760, 156);
+            this.lable5.Name = "lable5";
+            this.lable5.Size = new System.Drawing.Size(81, 31);
+            this.lable5.TabIndex = 2;
+            this.lable5.Text = "Time :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Moves :";
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.BackColor = System.Drawing.Color.Transparent;
+            this.lbTime.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTime.ForeColor = System.Drawing.Color.White;
+            this.lbTime.Location = new System.Drawing.Point(847, 162);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(32, 23);
+            this.lbTime.TabIndex = 4;
+            this.lbTime.Text = "60";
+            // 
+            // lbMoves
+            // 
+            this.lbMoves.AutoSize = true;
+            this.lbMoves.BackColor = System.Drawing.Color.Transparent;
+            this.lbMoves.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMoves.ForeColor = System.Drawing.Color.White;
+            this.lbMoves.Location = new System.Drawing.Point(117, 162);
+            this.lbMoves.Name = "lbMoves";
+            this.lbMoves.Size = new System.Drawing.Size(21, 23);
+            this.lbMoves.TabIndex = 5;
+            this.lbMoves.Text = "0";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Transparent;
+            this.btnStart.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(54)))), ((int)(((byte)(94)))));
+            this.btnStart.BorderRadius = 20;
+            this.btnStart.BorderThickness = 2;
+            this.btnStart.CheckedState.Parent = this.btnStart;
+            this.btnStart.CustomImages.Parent = this.btnStart;
+            this.btnStart.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(52)))), ((int)(((byte)(95)))));
+            this.btnStart.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.HoverState.Parent = this.btnStart;
+            this.btnStart.Location = new System.Drawing.Point(12, 615);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.ShadowDecoration.Parent = this.btnStart;
+            this.btnStart.Size = new System.Drawing.Size(170, 48);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.Transparent;
+            this.btnRestart.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(54)))), ((int)(((byte)(94)))));
+            this.btnRestart.BorderRadius = 20;
+            this.btnRestart.BorderThickness = 2;
+            this.btnRestart.CheckedState.Parent = this.btnRestart;
+            this.btnRestart.CustomImages.Parent = this.btnRestart;
+            this.btnRestart.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(52)))), ((int)(((byte)(95)))));
+            this.btnRestart.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.ForeColor = System.Drawing.Color.White;
+            this.btnRestart.HoverState.Parent = this.btnRestart;
+            this.btnRestart.Location = new System.Drawing.Point(709, 615);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.ShadowDecoration.Parent = this.btnRestart;
+            this.btnRestart.Size = new System.Drawing.Size(170, 48);
+            this.btnRestart.TabIndex = 7;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 1000;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // fmMemoryGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(891, 675);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbMoves);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lable5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "fmMemoryGame";
+            this.Text = "Memory Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lable5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Label lbMoves;
+        private Guna.UI2.WinForms.Guna2Button btnStart;
+        private Guna.UI2.WinForms.Guna2Button btnRestart;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ Memory Game/Memory Game Screen.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory_Game
+{
+    public partial class fmMemoryGame : Form
+    {
+        List<int> Numbers = new List<int>() { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+        string FirstChoice;
+        string SecondChoice;
+        int Tries;
+        List<PictureBox> PictureBoxes = new List<PictureBox>();
+        PictureBox PicA;
+        PictureBox PicB;
+        int TotalTime = 30;
+        int CountDownTime;
+        bool GameOver = false;
+        public fmMemoryGame()
+        {
+            InitializeComponent();
+            CustomWindow(Color.FromArgb(185, 52, 95), Color.FromArgb(51, 51, 51), Color.FromArgb(185, 52, 95), Handle);
+            LoadPictures();
+        }
+
+        private string ToBgr(Color c) => $"{c.B:X2}{c.G:X2}{c.R:X2}";
+
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        const int DWWMA_CAPTION_COLOR = 35;
+        const int DWWMA_BORDER_COLOR = 34;
+        const int DWMWA_TEXT_COLOR = 36;
+        public void CustomWindow(Color captionColor, Color fontColor, Color borderColor, IntPtr handle)
+        {
+            IntPtr hWnd = handle;
+            //Change caption color
+            int[] caption = new int[] { int.Parse(ToBgr(captionColor), System.Globalization.NumberStyles.HexNumber) };
+            DwmSetWindowAttribute(hWnd, DWWMA_CAPTION_COLOR, caption, 4);
+            //Change font color
+            int[] font = new int[] { int.Parse(ToBgr(fontColor), System.Globalization.NumberStyles.HexNumber) };
+            DwmSetWindowAttribute(hWnd, DWMWA_TEXT_COLOR, font, 4);
+            //Change border color
+            int[] border = new int[] { int.Parse(ToBgr(borderColor), System.Globalization.NumberStyles.HexNumber) };
+            DwmSetWindowAttribute(hWnd, DWWMA_BORDER_COLOR, border, 4);
+
+        }
+        
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            CountDownTime--;
+            lbTime.Text = CountDownTime.ToString();
+            if (CountDownTime < 1)
+            {
+                MakeGameOver("Times Up", "You Lose");
+                foreach (PictureBox x in PictureBoxes)
+                {
+                    if (x.Tag != null)
+                    {
+                        x.Image = Image.FromFile("pics/" + (string)x.Tag + ".jpg");
+                    }
+                }
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        void LoadPictures()
+        {
+            int LeftPos = 248;
+            int TopPos = 203;
+            int Rows = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                PictureBox NewPic = new PictureBox();
+                NewPic.Height = 67;
+                NewPic.Width = 87;
+                NewPic.Cursor = Cursors.Hand;
+                NewPic.BorderStyle = BorderStyle.FixedSingle;
+                NewPic.BackColor = Color.Transparent;
+                NewPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                NewPic.Click += NewPic_Click;
+                NewPic.MouseLeave += NewPic_MouseLeave;
+                PictureBoxes.Add(NewPic);
+                if (Rows < 3)
+                {
+                    Rows++;
+                    NewPic.Left = LeftPos;
+                    NewPic.Top = TopPos;
+                    this.Controls.Add(NewPic);
+                    LeftPos = LeftPos + 143;
+                }
+                if (Rows == 3)
+                {
+                    LeftPos = 248;
+                    TopPos += 110;
+                    Rows = 0;
+                }
+            }
+            RestartGame();
+        }
+
+        private void NewPic_MouseLeave(object sender, EventArgs e)
+        {
+            if (GameOver)
+            {
+                return;
+            }
+
+            if (FirstChoice != null && SecondChoice != null)
+            {
+                CheckPictures(PicA, PicB);
+            }
+
+            
+        }
+
+        private void NewPic_Click(object sender, EventArgs e)
+        {
+            if (GameOver)
+            {
+                return;
+            }
+            if (FirstChoice == null)
+            {
+                PicA = sender as PictureBox;
+                if (PicA.Tag != null && PicA.Image == null)
+                {
+                    PicA.Image = Image.FromFile("pics/" + (string)PicA.Tag + ".jpg");
+                    FirstChoice = (string)PicA.Tag;
+                }
+
+                return;
+            }
+            if (SecondChoice == null)
+            {
+                PicB = sender as PictureBox;
+                if (PicB.Tag != null && PicB.Image == null)
+                {
+                    PicB.Image = Image.FromFile("pics/" + (string)PicB.Tag + ".jpg");
+                    SecondChoice = (string)PicB.Tag;
+                }
+
+                return;
+            }
+            
+        }
+
+        void RestartGame()
+        {
+            
+            var randomList = Numbers.OrderBy(x => Guid.NewGuid()).ToList();
+            Numbers = randomList;
+            for (int i = 0; i < PictureBoxes.Count; i++)
+            {
+                PictureBoxes[i].Enabled = false; 
+                PictureBoxes[i].Image = null;
+                PictureBoxes[i].Tag = Numbers[i].ToString();
+            }
+            GameTimer.Stop();
+            Tries = 0;
+            lbMoves.Text = Tries.ToString();
+            lbTime.Text =  TotalTime.ToString();
+            GameOver = false;
+            CountDownTime = TotalTime;
+            btnStart.Enabled = true;
+        }
+
+        void CheckPictures(PictureBox A , PictureBox B)
+        {
+            if (FirstChoice == SecondChoice)
+            {
+                A.Tag = null;
+                B.Tag = null;
+            }
+
+            Tries++;
+            lbMoves.Text = Tries.ToString();
+
+            FirstChoice = null;
+            SecondChoice = null;
+
+            if (PictureBoxes.All(o => o.Tag == null))
+            {
+                MakeGameOver("Great Work", "You Win");
+                return;
+            }
+
+            foreach (PictureBox pics in PictureBoxes.ToList())
+            {
+                if (pics.Tag != null)
+                {
+                    pics.Image = null;
+                }
+            }
+            
+            
+        }
+
+        void MakeGameOver(string Message , string Title)
+        {
+            GameTimer.Stop();
+            GameOver = true;
+            MessageBox.Show(Message, Title , MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            foreach (PictureBox x in PictureBoxes)
+            {
+                x.Enabled = true;
+            }
+
+            GameTimer.Start();
+            btnStart.Enabled = false;
+        }
+    }
+}
